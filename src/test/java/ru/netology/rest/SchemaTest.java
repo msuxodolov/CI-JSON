4e3e3b5128fa670d492d150b9fa940752a65ea0f
Suhodolov Matwey <Msuxodolov18@gmail.com>
2024-03-14 12:45:46 +0300

    add build.gradle

---
+++ src/test/java/ru/netology/rest/SchemaTest.java
@@ -0,0 +1,29 @@
+package ru.netology.rest;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SchemaTest {
+
+    @Test
+    public void shouldSchemaTest() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("[0].currency", equalTo("RUB"));
+
+    }
+
+}
