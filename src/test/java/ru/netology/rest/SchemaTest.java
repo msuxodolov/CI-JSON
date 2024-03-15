fe48052f157cfb5bedbefc805dc83f673ed96c44
Suhodolov Matwey <Msuxodolov18@gmail.com>
2024-03-15 16:15:38 +0300

    add src/test/java/ru/netology/rest/SchemaTest.java

--- src/test/java/ru/netology/rest/SchemaTest.java
+++ src/test/java/ru/netology/rest/SchemaTest.java
@@ -21,8 +21,7 @@ public class SchemaTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-                .body("[0].currency", equalTo("RUB"));
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
 
     }
 
